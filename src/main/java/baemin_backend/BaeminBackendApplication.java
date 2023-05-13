02400767dd0589b347d42dec96dc087d9a0293c0
euna <christinejung10@daum.net>
2023-05-13 16:29:02 +0900

    initial commit

---
+++ src/main/java/baemin_backend/BaeminBackendApplication.java
@@ -0,0 +1,13 @@
+package baemin_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeminBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaeminBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/baemin_backend/BaeminBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package baemin_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeminBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
