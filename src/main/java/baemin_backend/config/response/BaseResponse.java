2cbb44b9d8e1f63bb218c6195798e0240634b331
euna <christinejung10@daum.net>
2023-05-14 23:39:34 +0900

    feat: 기본 Response 객체 구현

---
+++ src/main/java/baemin_backend/config/response/BaseResponse.java
@@ -0,0 +1,31 @@
+package baemin_backend.config.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Getter;
+
+import static baemin_backend.config.response.ResponseStatus.SUCCESS;
+
+@Getter
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
+public class BaseResponse<T> {
+
+    @JsonProperty("isSuccess")
+    private final Boolean isSuccess;
+
+    private final int code;
+
+    private final String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private final T result;
+
+    public BaseResponse(T result) {
+        this.isSuccess = SUCCESS.isSuccess();
+        this.code = SUCCESS.getCode();
+        this.message = SUCCESS.getMessage();
+        this.result = result;
+    }
+
+}
---
+++ src/main/java/baemin_backend/config/response/ResponseStatus.java
@@ -0,0 +1,35 @@
+package baemin_backend.config.response;
+
+import lombok.Getter;
+
+@Getter
+public enum ResponseStatus {
+
+    /**
+     * 1000: 요청 성공
+     */
+    SUCCESS(true, 1000, "요청에 성공하였습니다."),
+
+    /**
+     * 2000: Request 오류
+     */
+    NOT_FOUND(false, 2000, "잘못된 URL 요청입니다."),
+    BAD_PARAM_REQUEST(false, 2001, "잘못된 Parameter 요청입니다."),
+
+    /**
+     * 3000: Server, Database 오류
+     */
+    INTERNAL_SERVER_ERROR(false, 3000, "서버에서 오류가 발생하였습니다."),
+    DATABASE_ERROR(false, 3001, "데이터베이스에서 오류가 발생하였습니다.");
+
+    private final boolean isSuccess;
+    private final int code;
+    private final String message;
+
+    ResponseStatus(boolean isSuccess, int code, String message) {
+        this.isSuccess = isSuccess;
+        this.code = code;
+        this.message = message;
+    }
+
+}
