cd0b602a2132b6f0e88889acd6c424c003a58c77
euna <christinejung10@daum.net>
2023-05-17 21:25:53 +0900

    feat: HttpRequestMethodNotSupportedException를 처리하는 handler 추가

--- src/main/java/baemin_backend/common/exception/handler/BaseExceptionControllerAdvice.java
+++ src/main/java/baemin_backend/common/exception/handler/BaseExceptionControllerAdvice.java
@@ -2,13 +2,13 @@ package baemin_backend.common.exception.handler;
 
 import baemin_backend.common.exception.BadRequestException;
 import baemin_backend.common.response.BaseErrorResponse;
-import baemin_backend.common.response.BaseResponse;
 import baemin_backend.common.exception.InternalServerErrorException;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.TypeMismatchException;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -21,40 +21,26 @@ import static baemin_backend.common.response.status.BaseExceptionResponseStatus.
 public class BaseExceptionControllerAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(BadRequestException.class)
-    public BaseErrorResponse handle_BadRequest(BadRequestException e) {
+    @ExceptionHandler({BadRequestException.class, NoHandlerFoundException.class, TypeMismatchException.class})
+    public BaseErrorResponse handle_BadRequest(Exception e) {
         log.error("[handle_BadRequest]", e);
-        return new BaseErrorResponse(e.getExceptionStatus());
+        return new BaseErrorResponse(URL_NOT_FOUND);
     }
 
     // 위와 동일 (return ResponseEntity<>)
     /*
-    @ExceptionHandler(BadRequestException.class)
+    @ExceptionHandler({BadRequestException.class, NoHandlerFoundException.class, TypeMismatchException.class, HttpRequestMethodNotSupportedException.class})
     public ResponseEntity<BaseErrorResponse> handle_BadRequest(BadRequestException e) {
         log.error("[handle_BadRequest]", e);
         return ResponseEntity.badRequest().body(new BaseErrorResponse(e.getExceptionStatus()));
     }
      */
 
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler(InternalServerErrorException.class)
-    public BaseErrorResponse handle_InternalServerError(InternalServerErrorException e) {
-        log.error("[handle_InternalServerError]", e);
-        return new BaseErrorResponse(e.getExceptionStatus());
-    }
-
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(NoHandlerFoundException.class)
-    public BaseErrorResponse handle_NoHandlerFoundException(Exception e) {
-        log.error("[handle_NoHandlerFoundException]", e);
-        return new BaseErrorResponse(URL_NOT_FOUND);
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(TypeMismatchException.class)
-    public BaseResponse<Object> handle_TypeMismatchException(TypeMismatchException e) {
-        log.error("[handle_TypeMismatchException]", e);
-        return new BaseResponse<>(URL_NOT_FOUND);
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public BaseErrorResponse handle_HttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        log.error("[handle_HttpRequestMethodNotSupportedException]", e);
+        return new BaseErrorResponse(METHOD_NOT_SUPPORTED);
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -62,6 +48,13 @@ public class BaseExceptionControllerAdvice {
     public BaseErrorResponse handle_ConstraintViolationException(ConstraintViolationException e) {
         log.error("[handle_ConstraintViolationException]", e);
         return new BaseErrorResponse(BAD_REQUEST, e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(InternalServerErrorException.class)
+    public BaseErrorResponse handle_InternalServerError(InternalServerErrorException e) {
+        log.error("[handle_InternalServerError]", e);
+        return new BaseErrorResponse(e.getExceptionStatus());
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
