6d496b29ded5b02f7b7764795a3e0a567899fff7
euna <christinejung10@daum.net>
2023-05-19 14:31:14 +0900

    feat: JwtException

---
+++ src/main/java/baemin_backend/common/exception/JwtException.java
@@ -0,0 +1,16 @@
+package baemin_backend.common.exception;
+
+import baemin_backend.common.response.status.ResponseStatus;
+import lombok.Getter;
+
+@Getter
+public class JwtException extends RuntimeException {
+
+    private final ResponseStatus exceptionStatus;
+
+    public JwtException(ResponseStatus exceptionStatus) {
+        super(exceptionStatus.getMessage());
+        this.exceptionStatus = exceptionStatus;
+    }
+
+}
