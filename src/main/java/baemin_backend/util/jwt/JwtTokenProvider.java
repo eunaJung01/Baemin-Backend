d47ca58074a93b22297eb238da4e9630e8b9bb6a
euna <christinejung10@daum.net>
2023-05-20 23:42:58 +0900

    fix: JwtTokenProvider.createToken 메서드 signWith 인수 변경

--- src/main/java/baemin_backend/util/jwt/JwtTokenProvider.java
+++ src/main/java/baemin_backend/util/jwt/JwtTokenProvider.java
@@ -1,12 +1,13 @@
 package baemin_backend.util.jwt;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Slf4j
@@ -20,17 +21,19 @@ public class JwtTokenProvider {
     private static long JWT_EXPIRED_IN;
 
     public static String createToken(long userId) {
+        log.info("JWT key={}", JWT_SECRET_KEY);
+
         Claims claims = Jwts.claims().setSubject(String.valueOf(userId));
         Date now = new Date();
         Date validity = new Date(now.getTime() + JWT_EXPIRED_IN);
+        SecretKey secretKey = Keys.hmacShaKeyFor(JWT_SECRET_KEY.getBytes(StandardCharsets.UTF_8));
 
-        log.info("JWT key={}", JWT_SECRET_KEY);
         return Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(now)
                 .setExpiration(validity)
                 .claim("userId", userId)
-                .signWith(SignatureAlgorithm.HS256, JWT_SECRET_KEY)
+                .signWith(secretKey, SignatureAlgorithm.HS256)
                 .compact();
     }
 
