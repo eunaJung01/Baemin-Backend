bce914af370c7356707d0f5c7be5c4c29824356c
euna <christinejung10@daum.net>
2023-05-23 03:28:02 +0900

    refactor: BindingResultUtils.getErrorMessages 메서드에서 반환하는 message 형태 변경

--- src/main/java/baemin_backend/util/BindingResultUtils.java
+++ src/main/java/baemin_backend/util/BindingResultUtils.java
@@ -7,7 +7,7 @@ public class BindingResultUtils {
     public static String getErrorMessages(BindingResult bindingResult) {
         StringBuilder errorMessages = new StringBuilder();
         bindingResult.getAllErrors()
-                .forEach(error -> errorMessages.append("[").append(error.getDefaultMessage()).append("]"));
+                .forEach(error -> errorMessages.append(error.getDefaultMessage()).append(". "));
         return errorMessages.toString();
     }
 
