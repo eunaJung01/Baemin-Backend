5552c64b2fbf18f8fef8f7e62a1a441a2dfafc58
euna <christinejung10@daum.net>
2023-06-21 13:25:51 +0900

    chore: ConnectionTest 주석 처리 (EC2 내부에서 gradlew build 불가)

--- src/test/java/baemin_backend/ConnectionTest.java
+++ src/test/java/baemin_backend/ConnectionTest.java
@@ -1,30 +1,30 @@
 package baemin_backend;
 
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
-
-import static baemin_backend.config.secret.Connection.*;
-
-@Slf4j
-public class ConnectionTest {
-
-    @Test
-    void dataSourceDriverManager() throws SQLException {
-        // DriverManagerDataSource : 항상 새로운 커넥션을 획득한다.
-        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
-        useDataSource(dataSource);
-    }
-
-    private void useDataSource(DataSource dataSource) throws SQLException {
-        Connection con1 = dataSource.getConnection();
-        Connection con2 = dataSource.getConnection();
-        log.info("connection={}, class={}", con1, con1.getClass());
-        log.info("connection={}, class={}", con2, con2.getClass());
-    }
-
-}
+//import lombok.extern.slf4j.Slf4j;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.jdbc.datasource.DriverManagerDataSource;
+//
+//import javax.sql.DataSource;
+//import java.sql.Connection;
+//import java.sql.SQLException;
+//
+//import static baemin_backend.config.secret.Connection.*;
+//
+//@Slf4j
+//public class ConnectionTest {
+//
+//    @Test
+//    void dataSourceDriverManager() throws SQLException {
+//        // DriverManagerDataSource : 항상 새로운 커넥션을 획득한다.
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+//        useDataSource(dataSource);
+//    }
+//
+//    private void useDataSource(DataSource dataSource) throws SQLException {
+//        Connection con1 = dataSource.getConnection();
+//        Connection con2 = dataSource.getConnection();
+//        log.info("connection={}, class={}", con1, con1.getClass());
+//        log.info("connection={}, class={}", con2, con2.getClass());
+//    }
+//
+//}
